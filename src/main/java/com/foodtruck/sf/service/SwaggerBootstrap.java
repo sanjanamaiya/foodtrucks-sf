4b3bb6f453fe532115b486e64e2bceef70d98ae8
Sanjana Maiya <sanjanamaiya@users.noreply.github.com>
2015-08-04 03:08:04 -0700

    Update SwaggerBootstrap.java
    Set the right server name for base path

--- src/main/java/com/foodtruck/sf/service/SwaggerBootstrap.java
+++ src/main/java/com/foodtruck/sf/service/SwaggerBootstrap.java
@@ -16,7 +16,7 @@ public class SwaggerBootstrap extends HttpServlet
 		super.init(config);
 		BeanConfig beanConfig = new BeanConfig();
 		beanConfig.setVersion("1.0.0");
-		beanConfig.setBasePath("http://localhost:8080/foodtruckservice/v1");
+		beanConfig.setBasePath("http://ec2-52-27-9-65.us-west-2.compute.amazonaws.com:8080/foodtruckservice/v1");
 		beanConfig.setResourcePackage("com.foodtruck.sf.service");
 		beanConfig.setScan(true);
 		beanConfig.setTitle("San Francisco Food Trucks service");
